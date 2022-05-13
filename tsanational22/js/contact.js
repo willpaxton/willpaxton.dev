cb55943c154ec762c079ad60a7b193bc7bc490cf
willpaxton <43458264+willpaxton@users.noreply.github.com>
2022-05-13 11:35:02 -0400

    removed some files and updated some others

--- tsanational22/js/contact.js
+++ tsanational22/js/contact.js
@@ -1,4 +1,4 @@
-// These scripts are only used on the Contact page of the website
+// These scripts are used on the home page form
 var contactEmailTrue = false;
 var firstNameTrue = false 
 var lastNameTrue = false
@@ -44,6 +44,17 @@ function radioChecked(radioA, radioB) {
     }
 }
 
+// clears the form, kinda a demo submit
+function submitForm() {
+    document.getElementById('firstName').value = "";
+    document.getElementById('lastName').value = "";
+    document.getElementById('emailInput').value = "";
+    document.getElementById('phoneNumber').value = "";
+    document.getElementById('affiliationQuestion').value = "Choose an Option";
+    document.getElementById('radio1').checked = false;
+    document.getElementById('radio2').checked = false;
+}
+
 
 // This script runs when the form is submitted.
 function formSubmit() {
@@ -66,19 +77,39 @@ function formSubmit() {
             switch(phoneTrue) {
                 case true:
                     if (firstNameTrue==true && lastNameTrue==true && dropdownTrue==true && radioTrue == true) {
-                        alert("The form has been submitted.");
-                        document.getElementById('registrationForm').submit();
+                        // alert("The form has been submitted.");
+                        // document.getElementById('registrationForm').submit();
+                        formSuccess.style.display = "inline";
+                        formInvalid.style.display = "none";
+                        formNumber.style.display = "none";
+                        formEmail.style.display = "none";
+                        submitForm()
                     } else if (firstNameTrue==false || lastNameTrue==false || dropdownTrue==false || radioTrue == false) {
-                        alert("Please fill in all required sections of the form.")
+                        // alert("Please fill in all required sections of the form.")
+                        formSuccess.style.display = "none";
+                        formInvalid.style.display = "inline";
+                        formNumber.style.display = "none";
+                        formEmail.style.display = "none";
+                        
                     }
                     break;
                 case false:
-                    alert("Please enter a valid phone number or fill in any required sections.");
+                    // alert("Please enter a valid phone number or fill in any required sections.");
+                    formSuccess.style.display = "none";
+                    formInvalid.style.display = "none";
+                    formNumber.style.display = "inline";
+                    formEmail.style.display = "none";
+                    
                     break;
             }
             break;
         case false:
-            alert("Please enter a valid email address or fill in any required sections.");
+            // alert("Please enter a valid email address or fill in any required sections.");
+            formSuccess.style.display = "none";
+            formInvalid.style.display = "none";
+            formNumber.style.display = "none";
+            formEmail.style.display = "inline";
+            
             break;
         default:
             console.log("error");
