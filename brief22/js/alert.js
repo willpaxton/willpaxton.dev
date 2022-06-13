a7aab0b2e0ce7175b89ea1c6e1b1a465b406114e
Will Paxton <43458264+willpaxton@users.noreply.github.com>
2022-06-13 17:10:25 -0400

    removed an external link

---
+++ brief22/js/alert.js
@@ -0,0 +1,5 @@
+function gameAlert() {
+    alert("Developers Note: On the production site, this linked to an external game; however, that was not my worked so you're seeing this instead. :)")
+    console.log("teehee moment")
+}
+
